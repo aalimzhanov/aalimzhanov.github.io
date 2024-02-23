2702f794db6b729cc78a533f7c33c32201bfcda4
Adil <aalimzhanov@mta.ca>
2024-02-23 13:18:02 -0400

    Reworked the structure into 1 page.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-import loadAboutMe from "./about.js";
-import loadProjects from "./projects.js";
-import loadContactMe from "./contacts.js";
 
-document.getElementById("button-about-me").addEventListener("click", () => {
-	loadAboutMe();
-});
-
-document.getElementById("button-projects").addEventListener("click", () => {
-	loadProjects();
-});
-document.getElementById("button-contacts").addEventListener("click", () => {
-	loadContactMe();
-});
-loadAboutMe();
--- src/projects.js
+++
@@ -1,6 +0,0 @@
-const loadProjects = () => {
-	const content = `<div id="projects" class="content">This page is reserved for projects</div>`;
-	document.querySelector(".content-wrapper").innerHTML = content;
-};
-
-export default loadProjects;
